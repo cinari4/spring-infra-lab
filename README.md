024e3740a60bcdcc938ece6d81e28eebfa228405
cinari4 <cinari4@gmail.com>
2025-06-23 12:13:15 +0900

    init redis intra lab

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-infra-lab
---
+++ redis/src/test/java/com/github/cinari4/springinfralab/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cinari4.springinfralab.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
